5d626a487c630723ad3c76b49865caa2846dbf8f
c-shawn710 <c_shawn710@yahoo.com>
2024-09-26 20:49:11 -0700

    first-maven-project-ShoppingList

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Apple");
+        System.out.println("Banana");
+        System.out.println("Milk");
+        System.out.println("Bread");
+        System.out.println("Cheese");
+        System.out.println("Eggs");
+        System.out.println("Toilet Paper");
+        System.out.println("Oil");
+        System.out.println("Napkins");
+        System.out.println("Water Bottles");
+    }
+}
