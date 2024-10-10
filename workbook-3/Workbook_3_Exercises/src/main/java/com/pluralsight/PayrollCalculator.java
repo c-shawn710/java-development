3c8dcfc76a405d94b6a4460cb1ec14f62dd96e47
Shawn_Chung <c_shawn710@yahoo.com>
2024-10-10 01:01:21 -0700

    Updated PayrollCalc

--- workbook-3/Workbook_3_Exercises/src/main/java/com/pluralsight/PayrollCalculator.java
+++ workbook-3/Workbook_3_Exercises/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,24 +1,34 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class PayrollCalculator {
     public static void main(String[] args) {
 
         try {
-            FileReader employeeFile = new FileReader("C:\\Users\\c_sha\\Desktop\\pluralsight\\java-development\\workbook-3\\Workbook_3_Exercises\\src\\main\\java\\com\\pluralsight\\employees.csv");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the name of the file employee file to process: ");
+            String empFile = scanner.nextLine();
+            System.out.println("Enter the name of the payroll file to create: ");
+            String payFile = scanner.nextLine();
+            FileReader employeeFile = new FileReader(empFile);
             BufferedReader bufReader = new BufferedReader(employeeFile);
+            FileWriter writer = new FileWriter(payFile, true);
+            BufferedWriter bufWriter = new BufferedWriter(writer);
+
 
             String input;
             bufReader.readLine();
             while((input = bufReader.readLine()) != null) {
                 String[] columns = input.split("\\|");
                 Employee employee = new Employee(Integer.parseInt(columns[0]), columns[1], Double.parseDouble(columns[2]), Double.parseDouble(columns[3]));
-                System.out.printf("Employee: %s, Employee ID: %d, Gross Pay: $%.2f\n", employee.getName(), employee.getEmployeeId(), employee.getcalcGrossPay());
+                //System.out.printf("Employee: %s, Employee ID: %d, Gross Pay: $%.2f\n", employee.getName(), employee.getEmployeeId(), employee.getcalcGrossPay());
+                bufWriter.write(employee.getEmployeeId() + "|" + employee.getName() + "|" + employee.getcalcGrossPay() + "\n");
             }
+
             bufReader.close();
+            bufWriter.close();
         } catch (IOException e) {
             System.out.println("error");
             e.printStackTrace();
---
+++ workbook-3/Workbook_3_Exercises/src/main/java/com/pluralsight/SearchInventory.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+import java.util.ArrayList;
+
+public class SearchInventory {
+    public static void main(String[] args) {
+
+    }
+}
+
